a431f0a890eaa45de9cb8edd84097bbe2504e018
unknown <hammambenyounes@gmail.com>
2021-10-15 14:57:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Test from "./test";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyDcv6qPkWOYufuo-Z8_YtsC_p08IMxFebI",
+  authDomain: "wataniya-series.firebaseapp.com",
+  projectId: "wataniya-series",
+  storageBucket: "wataniya-series.appspot.com",
+  messagingSenderId: "492834824497",
+  appId: "1:492834824497:web:d1f1991b2f1f1b1f015010",
+  measurementId: "G-H4S532BT87",
+});
+
+export const auth = app.auth();
+export default app;
